aef2c6654d1ac8fe319abc924892f121ad5fb4cd
Максим Зарубин <maksimzarubin20002000@gmail.com>
2024-07-09 16:13:27 +0700

    add_inertia_vue_scss_zippy_etc

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: String,
+});
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/style.scss', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
+    resolve: {
+        alias: {
+            '@' : resolve(__dirname, 'resources/js'),
+        },
+    }
 });
